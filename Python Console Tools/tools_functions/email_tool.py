b2cf839ee2aec70cea00a456d357c0a179038295
colinarobinson <colinarobinson@gmail.com>
2012-12-24 14:06:16 -0800

    Update Python Console Tools/tools_functions/email_tool.py
    now supports multiple smtp services and address book module

--- Python Console Tools/tools_functions/email_tool.py
+++ Python Console Tools/tools_functions/email_tool.py
@@ -1,36 +1,64 @@
-
 import smtplib, sys
 from email.mime.text import MIMEText
-# email tool function
+
 def email_tool(self,args):
+    addon = False
+    if os.path.exists('tools_functions\\address_book.py'):
+        address_book = importlib.__import__('address_book')
+        addon = True
     message = input('Enter message body: ')
-    # Create a text/plain message
     msg = MIMEText(message)
     msg['Subject'] = 'Sent from %s' % sys.argv[0]
-    msg['From'], msg['To'] = input('\'From\' address? (gmail only): '), args[0]
-    # Send the message via gmail SMTP server
-    s = smtplib.SMTP('smtp.gmail.com', 587)
+    if args[0] == 'contact' and addon:
+        if len(args) > 2:
+            while len(args) > 2:
+                args[1] = args[1] + ' ' + args.pop(2)
+        name = args[1]
+        info = address_book.export(name,'email')
+        if info == -1:
+            print('Error: contact not found')
+            return;
+        else:
+            msg['To'] = info
+    elif args[0] == 'contact' and not addon:
+        print('Error: address_book module not found')
+        return;
+    else:
+        msg['To'] = args[0]
+    msg['From'] = input('\'From\' address: ')
+    mailHost = msg['From']
+    mailHost = mailHost[mailHost.index('@')+1:]
+    try:
+        s = smtplib.SMTP(serverDict[mailHost][0],serverDict[mailHost][1])
+    except KeyError:
+        print('Error: could not find server for ' + mailHost)
     s.ehlo()
     s.starttls()
     if self.settingsList['password'] is '':
-        s.login(msg['From'], input('Password: '))
+        try:
+            s.login(msg['From'], input('Password: '))
+        except smtplib.SMTPAuthenticationError:
+            s.login(input('\'From\': '), input('Password: '))
     else:
      try:
-        s.login(msg['From'], self.settingsList['password'])
+        s.login(msg['From'], self.simpleDecrypt(self.settingsList['password'][0]))
      except smtplib.SMTPAuthenticationError:
         s.login(msg['From'], input('Password: '))
     
     s.send_message(msg)
     s.quit()
 
+    
+serverDict = {  'gmail.com':('smtp.gmail.com', 587),
+                'hotmail.com':('smtp.live.com', 587),
+                'yahoo.com':('smtp.mail.yahoo.com', 995),
+                'msn.com':('smtp.live.com', 587)}
 #function reference, min args, max args, help info, case sensitive?, function name
 func_alias = 'email'
 func_info = (email_tool,
              1, 
              1, 
-            'emails the address supplied as a command line argument with an email from the user\'s gmail account. The subject heading is the calling program.',
+            'email <address> sends an email to the given address.',
             False,
             )
 settings = {'subject': ('T', False), 'anonymous': ('F', False),'password':('', True),'toAddress':('WERP', False)}
-
-
