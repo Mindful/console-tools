3f84dbc6199d81b617b8fd79c9ba48e192ac0f36
colinarobinson <colinarobinson@gmail.com>
2012-12-24 13:55:06 -0800

    Update Python Console Tools/tools_functions/emailToSMS.py
    now supports address book add module and multiple smtp services.

--- Python Console Tools/tools_functions/emailToSMS.py
+++ Python Console Tools/tools_functions/emailToSMS.py
@@ -1,7 +1,12 @@
-import smtplib, sys
+import smtplib, sys, os, importlib
 from email.mime.text import MIMEText
+from os import path
 
 def emailToSMS(self, args):
+    addon = False
+    if os.path.exists('tools_functions\\address_book.py'):
+        address_book = importlib.__import__('address_book')
+        addon = True
     carrierCodes = {
     'sprint': '@messaging.sprintpcs.com',
     'verizon': '@vtext.com',
@@ -12,16 +17,31 @@ def emailToSMS(self, args):
     'nextel': '@messaging.nextel.com',
     'boost': '@myboostmobile.com',
     'alltel': '@message.alltel.com'}
-    
-    number = args[0]
-    carrier = args[1]
+    if args[0] == 'contact' and addon:
+        if len(args) > 2:
+            while len(args) > 2:
+                args[1] = args[1] + ' ' + args.pop(2)
+        name = args[1]
+        info = address_book.export(name,'phone')
+        if info == -1:
+            print('Error: contact not found')
+            return;
+        else:
+            number, carrier = info
+    elif args[0] == 'contact' and not addon:
+        print('Error: address_book module not found')
+        return;
+    else:
+        number = args[0]
+        carrier = args[1]
+    print(addon)
     couldFindCarrier = True
     if carrier not in carrierCodes:
         couldFindCarrier = False
         print('Error: unrecognized carrier.')
     else:
         toAddress = number + carrierCodes[carrier]
-        fromAddress = input('\'From\' address (gmail only): ')
+        fromAddress = input('\'From\' address: ')
         password = input('Password: ')
         messages = parseTXT(input('Message: '))
         for segment in messages:
@@ -47,7 +67,12 @@ def send(self, msg, fromAddress, password, toAddress):
     message = MIMEText(msg)
     message['Subject'] = ''
     message['From'], message['To'] = fromAddress, toAddress
-    s = smtplib.SMTP('smtp.gmail.com', 587)
+    mailHost = msg['From']
+    mailHost = mailHost[mailHost.index('@')+1:]
+    try:
+        s = smtplib.SMTP(serverDict[mailHost][0],serverDict[mailHost][1])
+    except KeyError:
+        print('Error: could not find server for ' + mailHost)
     s.ehlo()
     s.starttls()
     if self.settingsList['password'] is '':
@@ -57,17 +82,22 @@ def send(self, msg, fromAddress, password, toAddress):
         s.login(message['From'], password)
     else:
      try:
-        s.login(message['From'], self.settingsList['password'][0])
+        s.login(message['From'], self.simpleDecrypt(self.settingsList['password'][0]))
      except smtplib.SMTPAuthenticationError:
         s.login(message['From'], password)
 
     s.send_message(message)
     s.quit()
-    
+
+#I'd also like to add the option for user-defined smtp settings...shouldn't be too hard
+serverDict = {  'gmail.com':('smtp.gmail.com', 587),
+                'hotmail.com':('smtp.live.com', 587),
+                'yahoo.com':('smtp.mail.yahoo.com', 995),
+                'msn.com':('smtp.live.com', 587)}
 func_alias = 'txt'
 func_info = (emailToSMS,
              2,
-             2,
+             4,
              'Sends text messages through gmail; txt <number> <carrier>',
              False,
              )
