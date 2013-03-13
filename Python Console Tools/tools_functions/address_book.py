6cb3061f283e33c4e4276f62e9eed4b3906485d5
colinarobinson <colinarobinson@gmail.com>
2013-03-12 17:38:55 -0700

    Changed to use ContactsList class from pytext
    Overhauled the structure to make more sense and be more efficient,
    and now the program uses the ContactsList class from Mindfulness's 
    PyText project to store the contacts during runtime.
    
    Also, the remove function works properly now.

--- Python Console Tools/tools_functions/address_book.py
+++ Python Console Tools/tools_functions/address_book.py
@@ -1,50 +1,30 @@
 # address book tool
 # make a file in the directory that contains an organized list of names, email addresses, and phone numbers (with service carrier)
 # data would be stored in such a way that the user can access it directly, or while using one of the other programs
-
+# NOTE: other programs will probably have to be re-written for this to work with them.
+# use the settings file as a template, but it probably isn't exactly right for what we need here.
 import os.path, os
 
 def contacts_view(address):
-    cFile = open(address,'r+')
-    contactsList = cFile.read()
-    contactsList = contactsList.split('\n')
-    for item in contactsList:
-        print(item + '\n')
-    cFile.close()
+    for item in contacts:
+        print(str(item) + '\n')
     
 def export(name, info_type, address):
-    cFile = open(address,'r+')
-    contactsList = cFile.read()
-    cFile.close()
-    contactsList = contactsList.split('\n')
-    i = 0
-    for item in contactsList:
-        contactsList[i] = item.split(':')
-        for field in contactsList[i]:
-          field = field.strip()
-        i += 1
-    contactsDict = dict()
-    for item in contactsList:
-        contactsDict[item[0]] = item[0:]
     try:
         if info_type == 'email':
-            return contactsDict[name][1]
+            return contacts[name].email
         elif info_type == 'phone':
-            return contactsDict[name][2],contactsDict[name][3]
+            return contacts[name].number,contactsDict[name].provider
         else:
             return -2
     except KeyError:
             return -1
     
 def contacts_find(address):
-    cFile = open(address,'r+')
-    contactsList = cFile.read()
-    cFile.close()
-    contactsList = contactsList.split('\n')
     print('Search for a contact or enter "exit" to exit.')
     search = input('Search: ').lower().strip()
     while search != 'exit':
-        results = [a for a in contactsList if a.startswith(search)]
+        results = [a for a in contacts if a.name.startswith(search)]
         if len(results) > 0:
             for item in results:
               print('\n' + item + '\n')
@@ -52,8 +32,7 @@ def contacts_find(address):
             print('Contact "'+ search + '" was not found.')
         search = input('Search: ')
     
-def contacts_write(address):
-    cFile = open(address,'a')
+def contacts_add(address):
     print('New Contact')
     name = input('Name: ').lower()
     email = input('Email: ').lower()
@@ -62,8 +41,7 @@ def contacts_write(address):
     if carrier not in carrierCodes:
         print('Invalid carrier')
         return
-    cFile.write('\n'+ name + ' : ' + email + ' : ' + phoneNumber + ' : ' + carrier)
-    cFile.close()
+    contacts.add(name, phoneNumber, carrier, email)
 
 def address_book_init(address):
     cFile = open(address,'w')
@@ -76,8 +54,18 @@ def contacts_menu(self, args):
     address = os.path.join(self.homeRoute, 'address_book.tool')
     if not os.path.exists(address):
         address_book_init(address)
+    
+    cFile = open(address,'r+')
+    contactsList = cFile.read().split('\n')
+    cFile.close()
+    
+    for item in contactsList:
+        if item is not "":
+            item = item.split(" : ")
+            contacts.add(item[0], item[2], item[3], item[1])
+    
     quit = False
-    commands = {'view': contacts_view, 'add': contacts_write, 'help': help, 'search': contacts_find, 'remove': remove}
+    commands = {'view': contacts_view, 'add': contacts_add, 'help': help, 'search': contacts_find, 'remove': remove}
     while not quit:
         command = input('(Contacts): ').strip().lower()
         if command == 'exit':
@@ -86,31 +74,82 @@ def contacts_menu(self, args):
             commands[command](address)
         else:
             print('Error: command "' + command + '" not found.')
-            
-def remove(address):
-    cFile = open(address,'r+')
-    contactsList = cFile.read()
-    cFile.close()
-    contact_name = input('Contact to remove from list: ')
-    contactsList = contactsList.split('\n')
-    contactsList_length = len(contactsList)
-    found = False
-    for item in contactsList:
-      print(item)
-      if item is not "" and item[:item.index(' : ')] == contact_name:
-        a = contactsList.index(item)
-        contactsList = contactsList[:a] + contactsList[a+1:]
-        found = True
-    if not found:
-        print('Contact \'' + contact_name + '\' not found')
-        return
+    
     cFile = open(address,'w')
-    writeList = ""
-    for item in contactsList:
-        writeList = writeList + '\n' + item
-    cFile.write(writeList)
+    for item in contacts:
+        cFile.write(str(item) + '\n')
     cFile.close()
+    
+def remove(address):
+    contact_name = input('Contact to remove from list: ')
+    del contacts[contact_name]
+    
+class ContactsList:
+    def __init__(self):
+        self.list = []
+        self.dict = {}
 
+    def __getitem__(self, index):
+        if type(index) is int:
+            return self.list[index]
+        elif type(index) is str:
+            return self.dict[index]
+        else: raise LookupError("Contact must be accessed by name or index")
+
+    def __delitem__(self, index):
+        if type(index) is int:
+            del self.dict[self.list[index].name]
+            del self.list[index]
+        elif type(index) is str:
+            self.list.remove(self[index]) #This must come first because we are using the dict to fetch the item
+            del self.dict[index]
+            #search for and remove from string
+
+    def __len__(self):
+        return len(self.list)
+
+    def __contains__(self, item):
+        'Searches for the contact by name, not object reference'
+        return item in self.dict.keys()
+
+    def fromList(self, list):
+        self.list = sorted(list)
+        for item in self.list:
+            self.dict[item.name]=item
+
+    def add(self, name, number, provider, email):
+        contact = Contact(name, number, provider, email)
+        lo = 0
+        hi = len(self.list) #insorting
+        while lo < hi:
+            mid = (lo+hi)//2
+            if contact < self.list[mid]: hi = mid
+            else: lo = mid +1
+        self.list.insert(lo, contact)
+        self.dict[name]=contact
+        return lo #This must return the index it inserts at, so we know where to place items in the treeview
+        
+    def addContact(self, c):
+        add(c.name, c.number, c.provider, c.email)
+    
+class Contact:
+    def __init__(self, name, number, provider, email):
+        self.name = name
+        self.email = email
+        self.number = number
+        self.provider = provider
+        
+    def __lt__(self, other):
+        return self.name < other.name
+        
+    def __gt__(self, other):
+        return self.name > other.name
+        
+    def __str__(self):
+        return self.name+" : "+self.email+" : "+self.number+" : "+self.provider
+    
+    
+    
 func_alias = 'contacts'
 func_info = (contacts_menu,
             0,
@@ -129,3 +168,5 @@ carrierCodes = {
     'nextel',
     'boost',
     'alltel'}
+    
+contacts = ContactsList()
